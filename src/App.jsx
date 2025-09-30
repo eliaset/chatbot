c954f53eb57735760443f934e8239b6bb9726c60
eliaset <eliasshamil@gmail.com>
2025-09-30 10:53:27 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Chatbot from "./Chatbot";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Chatbot />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Chatbot.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+function Chatbot() {
+  return (
+    <>
+      <div>
+        <input></input>
+        <button>Send</button>
+      </div>
+    </>
+  );
+}
+export default Chatbot;
