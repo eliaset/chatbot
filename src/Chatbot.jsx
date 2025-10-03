d923900b23b3a06d549b17eb01b666f8280c344a
eliaset <eliasshamil@gmail.com>
2025-10-03 18:50:09 +0300

    add external library for response

--- src/Chatbot.jsx
+++ src/Chatbot.jsx
@@ -1,10 +1,44 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import botPic from "./assets/bot.png";
+import profilePic from "./assets/profile.png";
 function Chatbot() {
+  const [input, setInput] = useState("");
+  const [message, setMessage] = useState([]);
+  const [botReplies, setBotReplies] = useState([]);
+
+  const change = (e) => {
+    setInput(e.target.value);
+  };
+  const send = () => {
+    if (input.trim() === "") return;
+    setMessage([...message, input]);
+    const reply = window.Chatbot.getResponse(input);
+    setBotReplies([...botReplies, reply]);
+    setInput("");
+  };
+
   return (
     <>
-      <div>
-        <input></input>
-        <button>Send</button>
+      <div className="all">
+        <div className="input-container">
+          <input className="input" onChange={change} value={input}></input>
+          <button className="send" onClick={send}>
+            Send
+          </button>
+        </div>
+        {message.map((m, index) => (
+          <div className="message-container" key={index}>
+            <div className="message">
+              <p key={index}>{m}</p>
+
+              <img src={profilePic} />
+            </div>
+            <div className="bot">
+              <img src={botPic} />
+              <p key={index}>{botReplies[index]}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
